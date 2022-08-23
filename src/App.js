470138e500d438fe20757d9f3aedd83cac77d3f9
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-08-24 01:13:41 +0530

    add validations, ui update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import CountryInfoCard from "./Component/CountryInfoCard/CountryInfoCard";
 import ErrorMsg from "./Component/ErrorMsg/ErrorMsg";
@@ -15,37 +17,88 @@ const searchByOptions = [
   "subregion",
 ];
 
+const searchOptions = {
+  "Please select a search option": [
+    "null",
+    "Search country, currency, capital, region...",
+  ],
+  Name: ["name", "Search India, Russia..."],
+  Code: ["alpha", "Search by country code... "],
+  Currency: ["currency", "Search rupee, dollar..."],
+  Language: ["lang", "Search odia, tamil, hindi..."],
+  Capital: ["capital", "Search Delhi, Moscow, Tokyo..."],
+  Region: ["region", "Search Asia, africa, antartica..."],
+  Subregion: ["subregion", "Search south asia, north america..."],
+};
+
 function App() {
   const [inputData, setInputData] = useState({
     inputText: "",
     searchOption: "",
   });
   const [countryList, setCountryList] = useState([]);
-  const [error, setError] = useState("")
+  const [error, setError] = useState("");
+  const [placeholderText, setPlaceholderText] = useState(
+    "Search country, currency, capital, region..."
+  );
 
   const handleChange = (event) => {
     setInputData({
       ...inputData,
       [event.target.name]: event.target.value,
     });
+
+    if (`${event.target.value}` in searchOptions) {
+      setInputData({
+        ...inputData,
+        [event.target.name]: `${searchOptions[event.target.value][0]}`,
+      });
+      setPlaceholderText(searchOptions[event.target.value][1]);
+    }
   };
+
+  console.log(placeholderText);
 
   const fetchApi = () => {
     const apiurl = `https://restcountries.com/v3.1/${inputData.searchOption}/${inputData.inputText}`;
-    axios.get(apiurl).then((response) => {
-      // console.log(response.data);
-      setCountryList(response.data);
-      setError("")
-    }).catch((error)=>{
-      setError(<ErrorMsg/>)
-    });
+    if (
+      inputData.searchOption === "Please select a search option" ||
+      inputData.searchOption === ""
+    ) {
+      toast.error("Please select a search option");
+    } else {
+      axios
+        .get(apiurl)
+        .then((response) => {
+          // console.log(response.data);
+          setCountryList(response.data);
+          setError("");
+          toast.success(
+            `Fetched ${response.data.length} item(s) Successfully!`
+          );
+        })
+        .catch((error) => {
+          setError(<ErrorMsg />);
+          toast.error(`Error: ${error}`);
+        });
+    }
   };
 
-
   return (
-    <div className="app-container h-full lg:h-screen flex flex-col items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-12">
-      <h2 className="font-semibold text-3xl p-2 mb-4">Search country info based on <span className="inline-block font-extrabold text-purple-500 p-2 rounded-lg bg-amber-400 underline underline-offset-4">search option</span>  and <span className="inline-block font-extrabold text-purple-500 p-2 rounded-lg bg-amber-400 underline underline-offset-4">user input</span>.</h2>
-      <div className="app-header bg-slate-100 lg:w-1/2 rounded-lg p-4 mb-2 shadow-lg">
+    <div className="app-container h-full lg:h-screen flex flex-col items-center p-12">
+      <ToastContainer />
+
+      <h2 className="font-semibold text-3xl text-center p-2 mb-4 entrance-animate">
+        Search country info based on{" "}
+        <span className="inline-block font-extrabold text-yellow-400 p-2 rounded-lg underline underline-offset-4">
+          search option
+        </span>{" "}
+        and{" "}
+        <span className="inline-block font-extrabold text-yellow-400 p-2 rounded-lg underline underline-offset-4">
+          user input
+        </span>
+      </h2>
+      <div className="app-header bg-slate-100 lg:w-1/2 rounded-lg p-4 mb-2 shadow-lg entrance-animate">
         <div className="flex flex-col items-center space-y-4">
           <div className="">
             <select
@@ -54,7 +107,7 @@ function App() {
               id=""
               className="outline-none p-4 rounded-lg shadow-md font-medium"
             >
-              {searchByOptions.map((item) => (
+              {Object.keys(searchOptions).map((item) => (
                 <option
                   value={
                     item !== "Please select a search option" ? item : "null"
@@ -88,8 +141,8 @@ function App() {
               <input
                 type="search"
                 id="default-search"
-                className="block p-4 pl-10 w-full text-md font-medium text-gray-900 bg-gray-50 rounded-lg outline-none border-2 border-gray-300 focus:ring-emerald-500 focus:border-emerald-500 transition-all ease-in-out duration-200"
-                placeholder="Search country, currency, capital, region..."
+                className="block p-2 pl-10 w-full text-xl font-medium text-gray-900 bg-gray-50 rounded-lg outline-none border-2 border-gray-300 focus:ring-emerald-500 focus:border-emerald-500 transition-all ease-in-out duration-200"
+                placeholder={placeholderText}
                 required
                 name="inputText"
                 onChange={handleChange}
@@ -97,7 +150,7 @@ function App() {
               />
               <button
                 type="submit"
-                className="text-white absolute right-2.5 bottom-2.5 bg-emerald-500 hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 transition-all ease-in-out duration-200"
+                className="text-white absolute right-2.5 bottom-1.5 bg-emerald-500 hover:bg-emerald-700 shadow-emerald-500/50 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 transition-all ease-in-out duration-200"
                 onClick={fetchApi}
               >
                 Search
@@ -105,24 +158,45 @@ function App() {
             </div>
           </div>
         </div>
-      <p className="mt-2 pt-4 text-center">
-        Made by{" "}
-        <a
-          className="underline underline-offset-2 text-emerald-500"
-          href="https://ashutoshdash.netlify.app/"
-        >
-          Ashutosh Dash
-        </a>
-      </p>
+        <p className="mt-2 pt-4 text-center">
+          Made by{" "}
+          <a
+            className="underline underline-offset-2 text-emerald-500"
+            href="https://ashutoshdash.netlify.app/"
+          >
+            Ashutosh Dash
+          </a>
+        </p>
       </div>
       {error}
-      <section className="country-results flex flex-col p-4 h-96 overflow-y-auto mt-12">
+
+      <div>
+        <p
+          className={
+            countryList.length == 0
+              ? "hidden"
+              : "font-bold text-gray-200 p-2"
+          }
+        >
+          Items({countryList.length})
+        </p>
+      </div>
+      <section className="country-results flex flex-col p-2 h-96 overflow-y-auto mt-12">
         {countryList.map(
           (item) => (
             // console.log(item.name.official);
             <CountryInfoCard
               key={item.name.official}
+              commonName={item.name.common}
               officialName={item.name.official}
+              flag={item.flags.png}
+              currencyName={Object.values(item.currencies)[0].name}
+              currencySymbol={Object.values(item.currencies)[0].symbol}
+              capital={item.capital}
+              subRegion={item.subregion}
+              population={Number(item.population).toLocaleString()}
+              timezones={item.timezones}
+              map={item.maps.googleMaps}
             />
           )
           // <p>{item.name.official}</p>
--- src/Component/CountryInfoCard/CountryInfoCard.js
+++ src/Component/CountryInfoCard/CountryInfoCard.js
@@ -1,16 +1,63 @@
-import React from 'react'
-import "./CountryInfoCard.css"
+import React from "react";
+import "./CountryInfoCard.css";
 
-function CountryInfoCard({commonName, officialName, currency, capital, subRegion, region, map, population, timezones, flag}) {
+function CountryInfoCard({
+  commonName,
+  officialName,
+  currencyName,
+  currencySymbol,
+  capital,
+  subRegion,
+  map,
+  population,
+  timezones,
+  flag,
+}) {
   return (
-    <div className='country-card bg-white shadow-lg rounded-lg p-8 my-2'>
-        <p>{officialName}</p>
-        <p>{currency}</p>
-        <p>{capital}</p>
-        <p>{region}</p>
-        
+    <div className="country-card bg-white shadow-lg rounded-lg p-8 my-2 max-w-3xl break-all">
+      <div className="flex items-center justify-between">
+        <img className="h-8 shadow-md border-2" src={flag} alt="" />
+        <p className="p-2 text-xl font-bold">
+          {officialName}
+          <span className="italic text-neutral-500">({commonName})</span>
+        </p>
+      </div>
+      <div className="flex flex-col justify-start md:flex-row md:justify-between md:space-x-4">
+        <div>
+          <p>
+            <span className="font-bold">Currency: </span>
+            <span>{currencyName}</span> <span>({currencySymbol})</span>
+          </p>
+          <p>
+            <span className="font-bold">Population: </span>
+            {population}
+          </p>
+          <p>
+            <span className="font-bold">Time Zones: </span>
+            {timezones}
+          </p>
+        </div>
+        <div>
+          <p>
+            <span className="font-bold">Capital: </span>
+            {capital}
+          </p>
+          <p>
+            <span className="font-bold">Region: </span>
+            {subRegion}
+          </p>
+          <p>
+            <a
+              className="cursor-pointer underline underline-offset-2 text-emerald-500"
+              href={map}
+            >
+              View Map
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CountryInfoCard
+export default CountryInfoCard;
