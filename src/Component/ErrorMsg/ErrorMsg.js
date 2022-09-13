2788e20886456a36fdf0aed0d9feae122b0eb1ad
Dinesh Rout <rddinesh779@gmail.com>
2022-09-13 21:43:20 +0530

    style: refactor UI

--- src/Component/ErrorMsg/ErrorMsg.js
+++ src/Component/ErrorMsg/ErrorMsg.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function ErrorMsg() {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="mt-4 flex flex-col justify-center items-center">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-20 w-20"
